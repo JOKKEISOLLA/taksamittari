c94a4d5f14be2c98f58e9b5efb62426cfad7ba95
JOKKEISOLLA <102411171+JOKKEISOLLA@users.noreply.github.com>
2022-04-13 10:55:01 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,18 +1,15 @@
 Config = {}
 
--- The currency prefix to be used
-Config.CurrencyPrefix = '�'
+-- Valuutta € / $
+Config.CurrencyPrefix = '€'
 
--- Enables or Disables the vehicle restriction
 Config.RestrictVehicles = true
 
--- Distance Measurement -- valid options are "mi" or "km". "mi" is default. If you
--- change this be sure to change RateSuffix as well
+-- Toimiiko kilometreinä = km / maileina = mi
 Config.DistanceMeasurement = 'km'
 
--- Rate Suffix
 Config.RateSuffix = '/km'
 
--- Which vehicles can not use the meter (if RestrictVehicles= true). By default
--- Bicycles, OffRoad and Emergency vehicles are disallowed
+
+-- Älä koske!
 Config.DisallowedVehicleClasses = {8, 9, 18}
